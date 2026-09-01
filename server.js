c39a5654eac0fcbaf609b136038fee46e3edf742
jabberdhami <jabberdhami@gmail.com>
2026-09-01 08:42:12 +0530

    Change server log to use 'localhost' instead of '0.0.0.0'

--- server.js
+++ server.js
@@ -467,5 +467,5 @@ fs.watch(BASE_DIR, { recursive: true }, (eventType, filename) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
